f3e88f12866605d50c17054111f9a3a868d68633
mariadenise123 <denise.oliveira.maria@escola.pr.gov.br>
2025-06-12 08:47:58 -0300

    tyjfyui

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criarCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criarCartao(
+    'Geografia',
+    'Qual a capital da frança?',
+    'A capital da frança é Paris'
+)
+
+criarCartao(
+    'Programação',
+    'O que é uma funçáo?',
+    ''
+)
+
+criarCartao(
+    categoria,
+    pergunta,
+    resposta
+)
